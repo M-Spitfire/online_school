f9e38ef719380f1d69c7feaa97964efcddaee847
M-Spitfire <xcw97@outlook.com>
2020-08-05 15:12:19 +0800

    MyBatisPlus创建和修改时间的自动填充

--- common/service_base/src/main/java/com/xcw/servicebase/handler/MyMetaObjectHandler.java
+++ common/service_base/src/main/java/com/xcw/servicebase/handler/MyMetaObjectHandler.java
@@ -1,4 +1,4 @@
-package com.xcw.servicebase.config.handler;
+package com.xcw.servicebase.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
